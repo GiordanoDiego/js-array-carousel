34ea9b7fc1c7c220a8377e394478d200e2a061fe
GiordanoDiego <diegomgiordano96@gmail.com>
2023-12-07 16:08:18 +0100

    mileston 2 completed

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ document.getElementById('idHtml').innerHTML = noveVariabileJS;
 
 */
 
+// INSERIRE IMG DENTRO DIV IMG-CONTAINER
+const imgContainer = document.querySelector('.img-container');
+console.log('imgContainer', imgContainer, typeof imgContainer);
+
+// creo un array che contiene tutte le stringhe da inserire nell'html
+const arrayImgHTML = [
+    '<img class="d-block" src="./img/04.webp" alt="">',
+    '<img class="" src="./img/01.webp" alt="">',
+    '<img class="" src="./img/02.webp" alt="">',
+    '<img class="" src="./img/03.webp" alt="">',
+    '<img class="" src="./img/05.webp" alt="">'
+]
+console.log('arrayImgHTML', arrayImgHTML, typeof arrayImgHTML);
+
+// creo un ciclo for che inserisce le stringhe dell'array dentro l'html
+
+for(let i=0; i < arrayImgHTML.length; i++){
+    imgContainer.innerHTML += arrayImgHTML[i];
+}
+
